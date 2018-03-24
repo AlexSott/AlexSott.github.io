17d696bf0b9b8e3b3d27565a6c9088393e812525
Admin <oleksandr.sot@gmail.com>
2018-03-24 14:47:49 +0200

    changes

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {
+	scrollFunction();
+	// this.console.log('test');
+};
+
+function scrollFunction() {
+	if (document.documentElement.scrollTop > 800) {
+		document.getElementById("scrollBtn").style.display = "block";
+	} else {
+		document.getElementById("scrollBtn").style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+$('html, body').animate({scrollTop: 0}, 2000);
